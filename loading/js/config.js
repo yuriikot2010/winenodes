3fca055bebb2a2109aa0eaee7b7e849c8a8ec635
yuriikot2010 <yuriikot2010@gmail.com>
2025-02-15 18:04:15 +0200

    2

---
+++ loading/js/config.js
@@ -0,0 +1,49 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Enjoy your stay!",
+  "This loading screen is made by Exsite."
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
