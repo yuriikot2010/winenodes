cf10326f987b8a094fb799db77f6ea0d11dfc7aa
yuriikot2010 <yuriikot2010@gmail.com>
2025-02-15 19:11:26 +0200

    4

--- loading/js/config.js
+++ loading/js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "";
+Config.title = "WineNodes";
 
 /**
  * Enable map text in the top left corner of the screen?
